2d8d21a30b29a1f9da02aee32cb3bf5f5ae86228
kyuontopelfst13 <80533545+kyuontopelfst13@users.noreply.github.com>
2025-01-31 00:34:15 +0000

    vertex API testing

---
+++ datasets/visionbase_text_test.jsx
@@ -0,0 +1,20 @@
+const {VertexAI} = require('@google-cloud/vertexai');
+
+/**
+ * TODO(developer): Update these variables before running the sample.
+ */
+async function generate_from_text_input(projectId = 'mlb-visionbase') {
+  const vertexAI = new VertexAI({project: projectId, location: 'us-central1'});
+
+  const generativeModel = vertexAI.getGenerativeModel({
+    model: 'gemini-1.5-flash-001',
+  });
+
+  const prompt =
+    "What's a good name for a flower shop that specializes in selling bouquets of dried flowers?";
+
+  const resp = await generativeModel.generateContent(prompt);
+  const contentResponse = await resp.response;
+  console.log(JSON.stringify(contentResponse));
+}
+generate_from_text_input();
